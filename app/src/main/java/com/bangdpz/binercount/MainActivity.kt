73b6a9697e71e13fff5df133889d33e8481a1cfd
franzydx <dapzfranzy@gmail.com>
2025-03-10 23:28:20 +0700

    Update: Layout

--- app/src/main/java/com/bangdpz/binercount/MainActivity.kt
+++ app/src/main/java/com/bangdpz/binercount/MainActivity.kt
@@ -3,27 +3,26 @@ package com.bangdpz.binercount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangdpz.binercount.databinding.ActivityMainBinding
 
 public class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
+    // private var _binding: ActivityMainBinding? = null
     
-    private val binding: ActivityMainBinding
-      get() = checkNotNull(_binding) { "Activity has been destroyed" }
+    // private val binding: ActivityMainBinding
+    // get() = checkNotNull(_binding) { "Activity has been destroyed" }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Inflate and get instance of binding
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+        // _binding = ActivityMainBinding.inflate(layoutInflater)
 
         // set content view to binding's root
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
-    
+    /*
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
+    }*/
 }
