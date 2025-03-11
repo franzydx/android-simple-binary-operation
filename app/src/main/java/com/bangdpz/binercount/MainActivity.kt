e4482714b755c3626c5014ef57fb2b5af5be7f70
franzydx <dapzfranzy@gmail.com>
2025-03-11 08:11:37 +0700

    Clean code

--- app/src/main/java/com/bangdpz/binercount/MainActivity.kt
+++ app/src/main/java/com/bangdpz/binercount/MainActivity.kt
@@ -8,40 +8,23 @@ import android.widget.TextView
 import android.widget.Toast
 
 public class MainActivity : AppCompatActivity() {
-
-    /* element on layput
-    binInput1
-    binInput2
-    addButton
-    subButton
-    resultView
-    */
     
     private lateinit var binaryInput1: EditText
     private lateinit var binaryInput2: EditText
     private lateinit var xaddButton: Button
     private lateinit var xsubButton: Button
     private lateinit var resultTextView: TextView
-    
-    // private var _binding: ActivityMainBinding? = null
-    
-    // private val binding: ActivityMainBinding
-    // get() = checkNotNull(_binding) { "Activity has been destroyed" }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inflate and get instance of binding
-        // _binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // set content view to binding's root
         setContentView(R.layout.activity_main)
         
         binaryInput1 = findViewById(R.id.binInput1)
-       binaryInput2 = findViewById(R.id.binInput2)
-      xaddButton = findViewById(R.id.addButton)
-       xsubButton = findViewById(R.id.subButton)
-      resultTextView = findViewById(R.id.resultView)
+        binaryInput2 = findViewById(R.id.binInput2)
+        xaddButton = findViewById(R.id.addButton)
+        xsubButton = findViewById(R.id.subButton)
+        resultTextView = findViewById(R.id.resultView)
       
       xaddButton.setOnClickListener() {
         val binary_1 = binaryInput1.text.toString()
@@ -51,8 +34,7 @@ public class MainActivity : AppCompatActivity() {
             resultTextView.text = "Hasil akhirnya adalah $sum"
         } else {
             Toast.makeText(this, "Input tidak valid!", Toast.LENGTH_SHORT).show()
-        }
-        
+        } 
       }
       
      xsubButton.setOnClickListener() {
@@ -64,30 +46,18 @@ public class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Input tidak valid!", Toast.LENGTH_SHORT).show()
         }
-        
-        
-        
       }
-      
     }
-    /*
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }*/
-    
-    // ngecek input user
+   
     private fun checkBinary(bin: String): Boolean {
         return bin.all { it == '0' || it == '1'}
     }
     
-    // fungis untuk penambahan
     private fun doPenambahan(bin1: String, bin2: String): String {
         val hasil = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2)
         return Integer.toBinaryString(hasil) 
     }
     
-    // fungsi untuk pengurangan
     private fun doPengurangan(bin1: String, bin2: String): String {
         val bit1 = Integer.parseInt(bin1, 2)
         val bit2 = Integer.parseInt(bin2, 2)
